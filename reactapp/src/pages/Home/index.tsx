56efad75aa72e11823b6b66b861c4d5f2b32d4b2
Thaís Silva <thaisdss.silva@gmail.com>
2022-10-08 06:39:33 -0300

    passei o projeto para typescript e tipei o componente, o estado e a resposta da API

--- reactapp/src/pages/Home/index.tsx
+++ reactapp/src/pages/Home/index.tsx
@@ -1,20 +1,30 @@
 import React, {useState, useEffect} from "react"
 import './style.css'
-import {Card} from '../../components/Card'
+import {Card, CardProps} from '../../components/Card'
+
+type ProfileResponse = {
+  name: string;
+  avatar_url: string;
+}
+
+type User = {
+  name: string;
+  avatar: string;
+}
 
 export function Home() {
   const [studentName, setStudentName] = useState("")
-  const [students, setStudents] = useState([])
-  const [user, setUser] = useState({name: '', avatar: ''})
+  const [students, setStudents] = useState<CardProps[]>([])
+  const [user, setUser] = useState<User>({} as User)
 
   function handleAddStudent(){
     const newStudent = {
       name: studentName,
-      time: new Date().toLocaleTimeString("pt-br", {
+      time: Number(new Date().toLocaleTimeString("pt-br", {
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
-      }),
+      })),
     };
 
     setStudents(prevState => [...prevState, newStudent])
@@ -35,7 +45,8 @@ export function Home() {
   useEffect(() => {
     async function fetchData(){
       const response = await fetch("https://api.github.com/users/thaisdss")
-      const data = await response.json()
+      const data = await response.json() as ProfileResponse;
+
       setUser({
         name: data.name,
         avatar: data.avatar_url
